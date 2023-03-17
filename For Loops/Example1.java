71b94d8fafdb81abf30688c427eb19691104b580
ServerLite <95834651+ServerLite@users.noreply.github.com>
2023-03-17 15:23:38 -0400

    Create Example1.java

---
+++ For Loops/Example1.java
@@ -0,0 +1,13 @@
+public class ForLoop extends ConsoleProgram {
+	public void run() {
+	
+		// Looping 10 times
+		
+		// int i = 0;          Starting point for the for loop
+		// i < 10;             Looping until 'i' reachs 10
+		// i++                 'i' increases by one
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i); // Displaying the variable 'i' from the for loop to the console
+		};
+	};
+};
