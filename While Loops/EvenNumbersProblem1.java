0e121057828b6402dbe1193dc027365ec68f77eb
ServerLite <95834651+ServerLite@users.noreply.github.com>
2023-03-17 15:40:01 -0400

    Create EvenNumbersProblem1.java

---
+++ While Loops/EvenNumbersProblem1.java
@@ -0,0 +1,13 @@
+public class EvenWhile extends ConsoleProgram {
+	public void run() {
+		
+		// Looping 500 times
+		
+		// int i = 0;          Starting value is 0
+		// i < 500             Keep looping until 'i' is 500
+		// i += 2               Increment the value 'i' by 2
+		for(int i = 0; i < 500; i+=2) {
+			System.out.println(i); // Outputting the 'i' variable
+		};
+	};
+};
