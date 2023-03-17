69975336eb6f103fb33ccf06220b837a6dff510b
ServerLite <95834651+ServerLite@users.noreply.github.com>
2023-03-17 15:31:41 -0400

    Update FactorialPractice.java

--- For Loops/FactorialPractice.java
+++ For Loops/FactorialPractice.java
@@ -4,6 +4,7 @@ public class Factorial extends ConsoleProgram {
 		
 		int factorial_number = readInt("Number to be factorialed: "); // Getting a number from the user
 		
+		// For looping from the "factorial_number" to 1
 		for (int i = factorial_number; i > 0; i--) {
 			result = result * i; // Multiplying the variable 'i' to the result
 		};
