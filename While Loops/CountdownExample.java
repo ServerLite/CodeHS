5cbb0c8f6951f83f4b209915e1bf430bd6f3a90b
ServerLite <95834651+ServerLite@users.noreply.github.com>
2023-03-17 15:37:07 -0400

    Create CountdownExample.java

---
+++ While Loops/CountdownExample.java
@@ -0,0 +1,12 @@
+public class WhileLoop extends ConsoleProgram {
+	public void run() {
+		
+		int i = 10; // Starting number for the countdown
+		
+		// This will keep looping until 'i' is equal to 0
+		while (i >= 0) {
+			System.out.println(l); // Output the integer 'i' to the console
+			i--; // Decrement the i by 1
+		};
+	}; 
+};
