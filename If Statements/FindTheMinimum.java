bb957639a9415298081c21286b8684d0bc0c3890
ServerLite <95834651+ServerLite@users.noreply.github.com>
2023-03-21 09:13:35 -0400

    Update FindTheMinimum.java

--- If Statements/FindTheMinimum.java
+++ If Statements/FindTheMinimum.java
@@ -9,7 +9,7 @@ public class FindMinimum extends ConsoleProgram {
 		// We have to take 3 integer inputs from the user
 		int first_integer = readInt("Input a random number: "); // Getting a number from the user.
 		int second_integer = readInt("Input another random number: "); // Getting another number from the user.
-		int third_integer = readInput("I am sorry, but input another random number: "); // Getting another number from the user.
+		int third_integer = readInt("I am sorry, but input another random number: "); // Getting another number from the user.
 		
 		// Check to see if the numbers are truly minimum
 		if (first_integer < minimum) {
